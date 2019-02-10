ce3d7879c17bd39dc9a9b74ce84a42c4e0cb2f2a
Smilga <smilga@Smilgas-iMac.local>
2019-02-09 22:29:01 -0800

    Rendering Products

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo";
 import BackgroundSection from "../components/Globals/BackgroundSection";
 import Info from "../components/Home/Info";
 import Menu from "../components/Home/Menu";
+import Products from "../components/Home/Products";
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
@@ -15,6 +16,7 @@ const IndexPage = ({ data }) => (
     />
     <Info />
     <Menu items={data.menu} />
+    <Products />
   </Layout>
 );
 
